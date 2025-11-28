f10be130185a3ca9d3206fe088027ca21fe4a851
mdesinno <mdesinno@gmail.com>
2025-11-28 15:57:22 +0100

    newsletter fix

--- netlify/functions/newsletter-intake.js
+++ netlify/functions/newsletter-intake.js
@@ -5,7 +5,7 @@ const { google } = require('googleapis');
 // ---------- Helper: Google Sheets ----------
 async function getSheets() {
   const jwt = new google.auth.JWT(
-    process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    process.env.GOOGLE_CLIENT_EMAIL,
     null,
     (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     ['https://www.googleapis.com/auth/spreadsheets']
